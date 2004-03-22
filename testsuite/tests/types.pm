d69656014169ec66fdc33d771ad1790f2ec8a4d1
Martin Vidner <mvidner@suse.cz>
2004-03-22 14:21:10 +0000

    added data type tests
    svn path=/trunk/perl-bindings/; revision=16673

--- testsuite/tests/types.pm
+++ testsuite/tests/types.pm
@@ -12,12 +12,14 @@ use YaST::YCP qw(Boolean);
 BEGIN { $TYPEINFO{bool1} = ["function", "boolean"]; }
 sub bool1
 {
+    my $class = shift;
     return 0;
 }
 
 BEGIN { $TYPEINFO{bool2} = ["function", "boolean"]; }
 sub bool2
 {
+    my $class = shift;
     return Boolean (0);
 }
 
