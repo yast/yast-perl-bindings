70a74750cfc31e3d8b303fbd993be6050fe12f27
Martin Vidner <mvidner@suse.cz>
2004-03-22 14:21:10 +0000

    added data type tests
    svn path=/trunk/perl-bindings/; revision=16673

---
+++ testsuite/tests/types2.pm
@@ -0,0 +1,29 @@
+#! /usr/bin/perl
+
+package types2;
+
+use warnings;
+use strict;
+
+our %TYPEINFO;
+
+
+use YaST::YCP qw(:DATA);
+BEGIN { $TYPEINFO{termloop} = ["function", "term", "term"]; }
+sub termloop
+{
+    my $class = shift;
+    return shift;
+}
+
+BEGIN { $TYPEINFO{termreverse} = ["function", "term", "term"]; }
+sub termreverse
+{
+    my $class = shift;
+    my $t = shift;
+
+    my @rargs = reverse @{$t->args};
+    return Term (reverse ($t->name), @rargs);
+}
+
+1;
