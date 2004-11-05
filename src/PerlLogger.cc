8f26cccd175a3d70ea09f17fa5d206b52a0b8749
Stanislav Višňovský <visnov@suse.cz>
2004-11-05 09:03:36 +0000

    forgotten files
    svn path=/trunk/perl-bindings/; revision=20203

---
+++ src/PerlLogger.cc
@@ -0,0 +1,50 @@
+/*---------------------------------------------------------------------\
+|								       |
+|		       __   __	  ____ _____ ____		       |
+|		       \ \ / /_ _/ ___|_   _|___ \		       |
+|			\ V / _` \___ \ | |   __) |		       |
+|			 | | (_| |___) || |  / __/		       |
+|			 |_|\__,_|____/ |_| |_____|		       |
+|								       |
+|				core system			       |
+|						     (C) SuSE Linux AG |
+\----------------------------------------------------------------------/
+
+  File:	      PerlLogger.cc
+
+  Author:     Stanislav Visnovsky <visnov@suse.cz>
+
+/-*/
+
+
+#include "PerlLogger.h"
+#include <ycp/ExecutionEnvironment.h>
+
+extern ExecutionEnvironment ee;
+
+void
+PerlLogger::error (string error_message)
+{
+    y2_logger (LOG_ERROR,"Perl",ee.filename ().c_str ()
+	,ee.linenumber (),"","%s", error_message.c_str ());
+}
+
+
+void
+PerlLogger::warning (string warning_message)
+{
+    y2_logger (LOG_ERROR,"Perl",ee.filename ().c_str ()
+	,ee.linenumber (),"","%s", warning_message.c_str ());
+}
+
+PerlLogger*
+PerlLogger::instance ()
+{
+    if ( ! m_perllogger )
+    {
+	m_perllogger = new PerlLogger ();
+    }
+    return m_perllogger;
+}
+
+PerlLogger* PerlLogger::m_perllogger = NULL;
