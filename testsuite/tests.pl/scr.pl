e06e0b75d91ba62187795dc61e546c7cbcd43d2f
Martin Vidner <mvidner@suse.cz>
2005-05-19 15:29:55 +0000

    Some more comments.
    svn path=/trunk/perl-bindings/; revision=23485

--- testsuite/tests.pl/scr.pl
+++ testsuite/tests.pl/scr.pl
@@ -8,6 +8,9 @@ YaST::YCP::Import ("SCR");
 
 my $path = ".target.size";
 my $arg = $0;
-my $size = -1; #SCR::Read ($path, $arg);
+my $size = -1;
+# This does not work yet: 
+# Couldn't find an agent to handle '.ests."pl/scr".pl'
+# Hmm, that's a path form of (t)ests.pl/scr.pl
+#my $size = SCR::Read ($path, $arg);
 print "Read ($path, $arg): $size\n";
-
